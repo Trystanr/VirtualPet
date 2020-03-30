2dc33ed9ede6f69d4515a5a99268289ff31d5871
Trystan <trystan.b@hotmail.com>
2020-03-30 11:38:58 +0200

    Skin Implementation
    Need to create Skins

--- VirtualPet/SettingsPage.xaml.cs
+++ VirtualPet/SettingsPage.xaml.cs
@@ -13,6 +13,7 @@ namespace VirtualPet {
             InitializeComponent();
 
             nameEntry.Text = pet.PetName;
+            SetCarouselIndex();
         }
 
         async void SettingsQuit(System.Object sender, System.EventArgs e) {
@@ -22,10 +23,43 @@ namespace VirtualPet {
 
                 pet.PetName = newPetName;
 
+                if (carSelect.Position == 0) {
+                    pet.PetSkin = "Man";
+                } else if (carSelect.Position == 1) {
+                    pet.PetSkin = "Woman";
+                }
+
                 await Navigation.PopModalAsync();
             }
-            
 
         }
+
+        void CarouselItemChanged(System.Object sender, Xamarin.Forms.CurrentItemChangedEventArgs e) {
+            Console.WriteLine("Item changed");
+            Console.WriteLine(carSelect.Position);
+        }
+
+
+        private void SetCarouselIndex() {
+
+            //Xamarin forms carousel does not set index on constructor, so I implemented this workaround hack
+            //Starts a timer of 100 miliseconds, but only runs it once
+
+            int characterSkin = 0;
+
+            if (pet.PetSkin == "Man") {
+                characterSkin = 0;
+            } else if (pet.PetSkin == "Woman") {
+                characterSkin = 1;
+            }
+
+            Device.StartTimer(TimeSpan.FromSeconds(0.1), () =>
+            {
+                carSelect.ScrollTo(characterSkin, animate: false);
+
+                return false;
+            });
+        }
+
     }
 }
